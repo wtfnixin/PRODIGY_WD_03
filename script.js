46e8b86fc80148b7063b7f5a62865fd3cc7c05b6
wtfnixin <nitinkumardontam@gmail.com>
2025-07-19 00:55:42 +0530

    added poosibilities of wins in array

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ const changeTurn = () => {
 
     
 const checkWin = () => {
+    let boxtexts = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0,1,2],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    wins.forEach(e =>{
+        
+    }
+
+    )
     
 }
 
