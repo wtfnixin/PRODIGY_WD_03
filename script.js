52328de8caf529b8307bfecf8d4fe5af4a5f5f2c
wtfnixin <nitinkumardontam@gmail.com>
2025-07-19 00:28:31 +0530

    added game logic

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 console.log("Welcome to TIC TAC TOE")
-let turn = new Audio("pop.mp3");
+let audioTurn = new Audio("pop.mp3");
 let gameover = new Audio("gameover.mp3");
 
 let turn = "X";
 
-const changeTurn = () => (
+const changeTurn = () => {
     return turn === "X" ? "O" : "X"
-)
-
-
-const checkWin = () => {
-
-
 }
+
+    
+const checkWin = () => {
+    
+}
+
 
 
 let boxes = document.getElementsByClassName("box");
 
-Array.from("boxes").forEach(element => {
-
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext')
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            
+        }
+    })
 
 
 })
