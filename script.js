d1cf67a4f204538cf2d7a998a659d67c5ee9af3a
wtfnixin <nitinkumardontam@gmail.com>
2025-07-18 18:22:36 +0530

    added new files

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Welcome to TIC TAC TOE")
+let turn = new Audio("pop.mp3");
+let gameover = new Audio("gameover.mp3");
+
+let turn = "X";
+
+const changeTurn = () => (
+    return turn === "X" ? "O" : "X"
+)
+
+
+const checkWin = () => {
+
+
+}
+
+
+let boxes = document.getElementsByClassName("box");
+
+Array.from("boxes").forEach(element => {
+
+    let boxtext = element.querySelector('.boxtext')
+
+
+})
+
+
