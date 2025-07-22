c3ebb11d25722006af84557a57289c6f9935585e
wtfnixin <nitinkumardontam@gmail.com>
2025-07-23 01:13:05 +0530

    completed

--- script.js
+++ script.js
@@ -23,11 +23,13 @@ const checkWin = () => {
     ]
 
     wins.forEach(e =>{
-        
-    }
+         if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== '')){
+            document.getElementsByClassName("info")[0].innerText = boxtexts[e[0]].innerText + " Won";
+            isgameover = true;
+            document.querySelector('.imgbox').classList.add('show');
+            gameover.play();}
 
-    )
-    
+    })  
 }
 
 
@@ -37,17 +39,32 @@ let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext')
     element.addEventListener('click', () => {
-        if (boxtext.innerText === '') {
+        if (boxtext.innerText === '' ) {
             boxtext.innerText = turn;
             turn = changeTurn();
             audioTurn.play();
             checkWin();
-            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
             
         }
     })
 
 
 })
+
+
+document.getElementById('reset').addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; 
+    isgameover = false;
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.imgbox').classList.remove('show'); 
+ })
+
 
 
